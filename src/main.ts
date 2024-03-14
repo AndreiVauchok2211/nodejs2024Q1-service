f524f286b796d7e7f0e5da20a05e9ef0262ed9b5
AndreiVauchok2211 <vauchokandrei@gmail.com>
2024-03-15 00:00:11 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getSwaggerDoc } from './services/swaggerDoc';
+import {serve, setup} from 'swagger-ui-express';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
+  const swaggerDocument = getSwaggerDoc();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.use('/doc', serve, setup(swaggerDocument))
+  await app.listen(PORT, () => console.log(`Listening port ${PORT}`));
 }
 bootstrap();
