94af631ccce611b30e3fdb5e1ca635ffbef47b4c
AndreiVauchok2211 <vauchokandrei@gmail.com>
2024-03-16 00:22:18 +0300

    feat: add user entity

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getSwaggerDoc } from './services/swaggerDoc';
 import {serve, setup} from 'swagger-ui-express';
+import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const swaggerDocument = getSwaggerDoc();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
   app.use('/doc', serve, setup(swaggerDocument))
   await app.listen(PORT, () => console.log(`Listening port ${PORT}`));
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    login: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    password: string;
+
+}
